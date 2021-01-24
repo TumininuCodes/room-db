290bc486acb748cdf8a76f54ad65adf06c0303fc
Tumi <oluwatumininuojo25@gmail.com>
2021-01-24 04:46:09 +0100

    room database implementation

--- app/src/main/java/com/tumininu/roomdatabase/db/Database.java
+++ app/src/main/java/com/tumininu/roomdatabase/db/Database.java
@@ -7,6 +7,7 @@ import com.tumininu.roomdatabase.model.Accessory;
 @androidx.room.Database(entities = {Accessory.class}, version = 1)
 
 public abstract class Database extends RoomDatabase {
+
     private static Database INSTANCE;
 
     public abstract AccessoryDao accessoryDao();
